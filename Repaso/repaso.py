be5af6b7369419fcf78322a2c06f14719afae2f4
Jose Fantasia <jose.fantasia@geneos.com.ar>
2021-03-01 00:29:44 -0300

    Ejemplos de Diccionarios - Anexo de comentarios

--- Repaso/repaso.py
+++ Repaso/repaso.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#Ejercicio: Convertir una cadena de texto con unidades de información a un diccionario
+#Ejercicio 1: Convertir una cadena de texto con unidades de información a un diccionario
 
 texto='123 libro 20;324 cuaderno 35;425 lápiz 50;284 regla 25'
 lista_texto=texto.split(';')
@@ -12,26 +12,34 @@ for e in lista_texto:
     clave=int(dato[0])
     nomb=dato[1]
     cant=int(dato[2])
+    # Crear la entreda en el diccionario
     d[clave]=[nomb,cant]
 # Imprimir el diccionario de valores
 print(d)
 
 
+#Ejercicio 2: crear un diccionario con lectura de datos ingresados por pantalla
 dic={}
 n=int(input('Cuantos datos desea cargar: '))
 
+# Itera la cantidad de veces ingresada para la carga de datos
 for i in range(n):
+    # Solicita datos
     cla=int(input('Clave: '))
     nom=input('Nombre: ')
     ed=int(input('Edad: '))
+    # Crear la entreda en el diccionario
     dic[cla]=[nom,ed]
     print('\nSalida: ')
 
+# Imprime los datos ingresados
 for cod in dic:
     print('\nNombre: ',dic[cod][0])
     print('Edad:',dic[cod][1])
 
+
 x=int(input('Clave para buscar: '))
+# Consulta si la clave solicitada se encuentra como clave del diccionario
 if x in dic:
     print('Nombre: ',dic[x][0])
     print('Edad:',dic[x][1])
